b13b360414ec4b0af57a2c74eebb52d7e49ca406
tmo macbook <trrl.mo@hotmail.com>
2022-09-08 11:58:26 -0400

    Implement path finding for arbitrary grid dimensions.

--- dp/unique-paths/test/uniquePaths.test.js
+++ dp/unique-paths/test/uniquePaths.test.js
@@ -10,6 +10,15 @@ describe('Calculations from uniquePaths function', () => {
     assert.equal(possiblePaths, 1);
   });
 
+  it('Finds 3 paths for a 3 x 2 grid.', () => {
+    const m = 3;
+    const n = 2;
+    const possiblePaths = uniquePaths(m, n);
+
+    assert.equal(possiblePaths, 3);
+  });
+
+
   it('Finds 28 paths for a 3 x 7 grid.', () => {
     const m = 3;
     const n = 7;
--- dp/unique-paths/uniquePaths.js
+++ dp/unique-paths/uniquePaths.js
@@ -1,12 +1,20 @@
 const uniquePaths = (m, n, currentPosition={x:0,y:0}, cache={}) => {
-  const val = cache[JSON.stringify(currentPosition)];
-  if (val) { return val }
+  const coordinates = JSON.stringify(currentPosition);
+  if (cache[coordinates]) { return cache[coordinates] };
 
-  const isAdjacentToEnd = currentPosition.x + 1 === m - 1 || currentPosition.y + 1 === n - 1;
+  const isAdjacentToEnd = currentPosition.x === m - 1 || currentPosition.y === n - 1;
   if (isAdjacentToEnd) {
-    cache[JSON.stringify(currentPosition)] = 1;
+    cache[coordinates] = 1;
     return 1;
   };
+
+  const rightNeighbor = { x: currentPosition.x + 1, y: currentPosition.y };
+  const neighborBelow = { x: currentPosition.x, y: currentPosition.y + 1 };
+  let possiblePaths = uniquePaths(m, n, rightNeighbor, cache) + uniquePaths(m, n, neighborBelow, cache);
+
+  cache[coordinates] = possiblePaths
+
+  return possiblePaths;
 };
 
 export { uniquePaths };
