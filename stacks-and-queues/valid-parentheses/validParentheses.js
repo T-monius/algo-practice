629d5593bb9a85b838d045f3a4873335078ca2c8
tmo macbook <trrl.mo@hotmail.com>
2022-09-23 14:26:10 -0400

    Refactor parentheses validation.

--- stacks-and-queues/valid-parentheses/validParentheses.js
+++ stacks-and-queues/valid-parentheses/validParentheses.js
@@ -1,5 +1,16 @@
-const CLOSERS =  { ')':'(', ']':'[', '}':'{' };
-const OPENERS = { '(':true, '[':true, '{':true }
+const CLOSERS_TO_OPENER_MAP =  { ')':'(', ']':'[', '}':'{' };
+const OPENERS = /[\(\[\{]/;
+const CLOSERS = /[\)\]\}]/;
+
+const handleIsParen = (char, stack) => {
+    if( OPENERS.test(char) ) {
+      stack.push(char);
+    };
+    const lastSeenOpener = stack[stack.length - 1];
+    if( CLOSERS_TO_OPENER_MAP[char] === lastSeenOpener) {
+      stack.pop();
+    };
+};
 
 const isValid = (str) => {
   if (str.length > 104) { return false };
@@ -8,16 +19,8 @@ const isValid = (str) => {
 
   for(let idx = 0; idx < str.length; idx += 1) {
     const char = str[idx];
-    if( !CLOSERS[char] && !OPENERS[char] ) {
-      return false;
-    };
-    if( OPENERS[char] ) {
-      seenOpeners.push(char);
-      continue;
-    }
-    const lastSeenOpener = seenOpeners[seenOpeners.length - 1];
-    if( CLOSERS[char] === lastSeenOpener) {
-      seenOpeners.pop();
+    if( CLOSERS.test(char) || OPENERS.test(char) ) {
+      handleIsParen(char, seenOpeners);
       continue;
     };
 
