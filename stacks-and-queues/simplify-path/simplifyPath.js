a7f89522ec021ca18679fffe9137efd46e2a0165
tmo macbook <trrl.mo@hotmail.com>
2022-09-30 16:09:00 -0400

    Add boilerplate for simplifyPath and tests.

---
+++ stacks-and-queues/simplify-path/simplifyPath.js
@@ -0,0 +1,5 @@
+const simplifyPath = (path) => {
+  //..
+};
+
+export { simplifyPath };
---
+++ stacks-and-queues/simplify-path/test/simplifyPath.test.js
@@ -0,0 +1,32 @@
+import assert from 'assert';
+import { simplifyPath } from '../simplifyPath.js';
+
+describe('The simplifyPath function', () => {
+  it('Does not allow trailing slashes', () => {
+    const unixLikePath = '/home/';
+    const canonicalLikePath = simplifyPath(unixLikePath);
+
+    assert.equal(canonicalLikePath, '/home');
+  });
+
+  it('Does not include double periods (dir ref)', () => {
+    const unixLikePath = '/../';
+    const canonicalLikePath = simplifyPath(unixLikePath);
+
+    assert.equal(canonicalLikePath, '/');
+  });
+
+  it('Disallows multiple consecutive slashes', () => {
+    const unixLikePath = '/home//foo/';
+    const canonicalLikePath = simplifyPath(unixLikePath);
+
+    assert.equal(canonicalLikePath, '/home/foo');
+  })
+
+  it('Treats more than two consecutive periods as a name', () => {
+    const unixLikePath = '/.../'
+    const canonicalLikePath = simplifyPath(unixLikePath);
+
+    assert.equal(canonicalLikePath, '/...');
+  });
+});
