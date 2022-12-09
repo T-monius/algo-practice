e6e1183d6eae3ecdbeb19f58d5d4dbd500071243
tmo macbook <trrl.mo@hotmail.com>
2022-12-09 12:21:04 -0500

    Refactor diamond printer

--- challenges/medium/ruby/diamond/10_diamond.rb
+++ challenges/medium/ruby/diamond/10_diamond.rb
@@ -1,43 +1,36 @@
-SPACE = ' '
-FIRST_ALPHA = 'A'
-
 class Diamond
-  def self.make_diamond(letter)
-    current_letter = FIRST_ALPHA
-    og_offset = letter.ord - FIRST_ALPHA.ord
-    iterations = og_offset * 2 + 1
+  SPACE = ' '
+  FIRST_ALPHA = 'A'
+
+  def self.make_diamond(input_letter)
+    letter = FIRST_ALPHA
+    og_offset = input_letter.ord - FIRST_ALPHA.ord
+    rows = og_offset * 2
     diamond = ''
-    idx = 0
 
-    while idx < iterations
-      offset_to_input_letter = letter.ord - current_letter.ord
-      offset_to_first = current_letter.ord - FIRST_ALPHA.ord
-      inner_spaces = current_letter == FIRST_ALPHA ? 0 : offset_to_first * 2 - 1
-      outer_spaces = offset_to_input_letter
-
-      if inner_spaces == 0
-        row = "#{SPACE * outer_spaces}" \
-          "#{current_letter}" \
-          "#{SPACE * outer_spaces}\n"
-      else
-        row = "#{SPACE * outer_spaces}" \
-          "#{current_letter}" \
-          "#{SPACE * inner_spaces}" \
-          "#{current_letter}" \
-          "#{SPACE * outer_spaces}\n"
-      end
-
+    0.upto(rows) do |idx|
+      row = Diamond.make_row(letter, input_letter)
       diamond += row
 
-      if idx < og_offset
-        current_letter = current_letter.next
-      else
-        current_letter = (current_letter.ord - 1).chr
-      end
-
-      idx += 1
+      letter = idx < og_offset ? letter.next : (letter.ord - 1).chr
     end
 
     diamond
+  end
+
+  def self.make_row(letter, input_letter)
+    offset_to_input_letter = input_letter.ord - letter.ord
+    offset_to_first = letter.ord - FIRST_ALPHA.ord
+    inner_spaces = letter == FIRST_ALPHA ? 0 : offset_to_first * 2 - 1
+    outer_spaces = offset_to_input_letter
+
+    if inner_spaces == 0
+      row = "#{SPACE * outer_spaces}#{letter}#{SPACE * outer_spaces}\n"
+    else
+      row = "#{SPACE * outer_spaces}#{letter}#{SPACE * inner_spaces}" \
+        "#{letter}#{SPACE * outer_spaces}\n"
+    end
+
+    row
   end
 end
